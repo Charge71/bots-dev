42460baa034882de8c8d4618801dfcf835a75543
Diego <Diego@192.168.1.132>
2016-06-15 20:25:55 +0200

    fix remove

--- src/main/java/com/charge71/telegram/bots/RssUpdateBot.java
+++ src/main/java/com/charge71/telegram/bots/RssUpdateBot.java
@@ -101,9 +101,9 @@ public class RssUpdateBot extends PlatformApiAware implements RssHandler {
 			for (int i = 0; i < feeds.length; i++) {
 				ObjectNode btn = b1.addArray().addObject();
 				btn.put("text", feeds[i].getName());
-				btn.put("callback_data", "remove_" + feeds[i].getName());
+				btn.put("callback_data", "remove_" + i);
 			}
-			client.sendButton(chatId, messages.getMessage(user.getLang(), "remove"), buttons.toString());
+			client.sendButtons(chatId, messages.getMessage(user.getLang(), "remove"), buttons.toString());
 		} else {
 			client.sendMessage(chatId, messages.getMessage(user.getLang(), "nofav", user.getFirstName()));
 		}
@@ -144,18 +144,14 @@ public class RssUpdateBot extends PlatformApiAware implements RssHandler {
 		RssUser user = mongoTemplate.findById(fromId, RssUser.class);
 		String data = json.get("callback_query").get("data").asText();
 		if (data.startsWith("remove_")) {
-			String name = data.substring(7);
+			int index = Integer.parseInt(data.substring(7));
 			RssSubscriptions subs = mongoTemplate.findById(fromId, RssSubscriptions.class);
 			RssFeed[] feeds = subs.getFeeds();
-			for (int i = 0; i < feeds.length; i++) {
-				if (feeds[i].getName().equals(name)) {
-					subs.setFeeds(ArrayUtils.remove(feeds, i));
-					log.debug("Removed " + feeds[i].getUrl() + " from " + fromId);
-					client.sendMessage(chatId, messages.getMessage(user.getLang(), "rssremove", feeds[i].getName()));
-					break;
-				}
-			}
+			RssFeed feed = subs.getFeeds()[index];
+			subs.setFeeds(ArrayUtils.remove(feeds, index));
 			mongoTemplate.save(subs);
+			client.sendMessage(chatId, messages.getMessage(user.getLang(), "rssremove", feed.getName()));
+			log.debug("Removed " + feed.getUrl() + " from " + fromId);
 		}
 	}
 
